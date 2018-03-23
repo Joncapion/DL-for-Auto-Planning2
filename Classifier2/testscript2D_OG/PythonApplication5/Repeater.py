152206e5fa89560f9b29e9759bf4674e76f54725
Joncapion <jon@hedenquist.dk>
2018-03-23 09:47:57 +0100

    changed somestuff

---
+++ Classifier2/testscript2D_OG/PythonApplication5/Repeater.py
@@ -0,0 +1,23 @@
+#Repeater.py runs the network x times with y steps and saves it every z runs.
+import sys
+import os
+import shutil
+import testscript
+if __name__ == "__main__":
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+    z = int(sys.argv[3])
+    # call = "python testscript.py " + str(y)
+    storage_location = os.environ['HOME']+"/storage/Classifier2/2D/OG/"
+    testscript.numOfSteps=y
+    for i in range(0,x):
+        # os.system(call)
+        testscript.main([])
+        
+        if (i%z == z-1 or i == x-1):
+            shutil.copytree("/tmp/Classifier2/2D/OG", storage_location+str(y*(i+1)), ignore=None)
+        else:
+            os.makedirs(storage_location+str(y*(i+1)))
+        shutil.copy("./results.txt",storage_location+str(y*(i+1)))
+        shutil.copy("./smallresults.txt",storage_location+str(y*(i+1)))
+        shutil.copy("./largeresults.txt",storage_location+str(y*(i+1)))
