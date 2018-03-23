2dd7cc9ac465d2b9cf0a870d5a0cb252c26ce8ea
Joncapion <jon@hedenquist.dk>
2018-03-23 03:58:27 +0100

    fixed repeater

--- Classifier2/testscript2D/PythonApplication5/Repeater.py
+++ Classifier2/testscript2D/PythonApplication5/Repeater.py
@@ -8,14 +8,14 @@ if __name__ == "__main__":
     y = int(sys.argv[2])
     z = int(sys.argv[3])
     # call = "python testscript.py " + str(y)
-    storage_location = os.environ['HOME']+"/storage/Classifier2/2D/"
+    storage_location = os.environ['HOME']+"/storage/Classifier2/2D/OG/"
     testscript.numOfSteps=y
     for i in range(0,x):
         # os.system(call)
         testscript.main([])
         
         if (i%z == z-1 or i == x-1):
-            shutil.copytree("/tmp/Classifier2/2D", storage_location+str(y*(i+1)), ignore=None)
+            shutil.copytree("/tmp/Classifier2/2D/OG", storage_location+str(y*(i+1)), ignore=None)
         else:
             os.makedirs(storage_location+str(y*(i+1)))
         shutil.copy("./results.txt",storage_location+str(y*(i+1)))
